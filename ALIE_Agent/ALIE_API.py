86ea3232139eb6ba4548b1d506d10f06d0f1e659
Luis Alejandro Bravo Ferreira <luisalejandrobravoferreira@gmail.com>
2024-09-23 16:29:51 -0500

    Changed port from 6000 to 3000

--- ALIE_Agent/ALIE_API.py
+++ ALIE_Agent/ALIE_API.py
@@ -79,7 +79,7 @@ if __name__ == "__main__":
     import sys
     sys.stdout.reconfigure(line_buffering=True) # Para que los prints se muestren en tiempo real
 
-    app.run(host='0.0.0.0', port=6000, threaded=True, debug=True)
+    app.run(host='0.0.0.0', port=3000, threaded=True, debug=True)
     # Threaded = True para que el servidor pueda manejar múltiples solicitudes simultáneamente
     # Debug = True para que el servidor se reinicie automáticamente después de cada cambio en el código
     
